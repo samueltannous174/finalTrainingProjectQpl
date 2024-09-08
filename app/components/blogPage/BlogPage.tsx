2cd92d86a2a404321ef7f6016f17cd4a3e692f08
samuel <samuel.tannous174@gmail.com>
2024-09-08 11:04:43 +0300

    added

--- app/components/blogPage/BlogPage.tsx
+++ app/components/blogPage/BlogPage.tsx
@@ -18,7 +18,7 @@ function BlogPage({ blogData, content  }: BlogPageProps) {
     const mode = searchParams.get('mode');
     return (
         <main className="box-border mx-auto flex flex-col min-h-screen items-start">
-            <section className="flex flex-col  p-4 md:p-0 mx-auto md:mx-0">
+            <section className="flex flex-col max-w-[100%] p-4 md:p-0 mx-auto md:mx-0">
                 {mode === 'add' ? (
                     <p className={`text-lg text-black-400 leading-relaxed mb-4 break-words flex-wrap max-w-[298px] lg:max-w-[398px] xl:max-w-[598px] text-[13px] md:max-w-[298px]`}>
                         <ClientOnly>
@@ -27,7 +27,7 @@ function BlogPage({ blogData, content  }: BlogPageProps) {
                     </p>
                 ) : (
                     <>
-                    <p className={`text-lg leading-relaxed mb-4 break-words flex-wrap p-0 w-[70%] lg:w-[50%] text-[13px] self-center mt-[80px]  `}>
+                    <p className={`text-lg leading-relaxed mb-4 break-words flex-wrap  p-0 w-[70%] lg:w-[50%] text-[13px] self-center mt-[80px]  `}>
                         <ClientOnly>
                             {() => !(blogData) || blogData.paragraph && <HtmlRenderer htmlString={blogData.paragraph}/>}
                         </ClientOnly>
